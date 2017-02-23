efb3fb9d8803b7cbba960ea1b1ef70cce07f7aea
Michael Odintsov <michael@datarobot.com>
2017-02-23 13:20:09 +0200

    комментарии и упрощения к общеупотребительному коду

--- common_ds.py
+++ common_ds.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
@@ -11,6 +12,34 @@ cm_bright = ListedColormap(['#FF0000', '#0000FF'])
 
 
 def cv_optimize(clf, parameters, X, y, n_jobs=1, n_folds=5, score_func=None):
+    """ Оптимизируем гиперпараметры классификатора с помощью Grid Search.
+    Перебираем все сочетания предложенных параметров и смотрим какое из них даст наилучший
+    результат (в среднем после кроссвалидации).
+
+    Parameters
+    ----------
+    clf : sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin
+        Объект-классификатор. Предполагается имплементация score и fit методов.
+    parameters : dict
+        Перебираемые параметры в виде {параметр1:[значение1_1, значение1_2],параметр2:[...]}
+    X : numpy.ndarray
+        Список спиской, данные признаков классификатора. Каждый элемент списка представляет
+        запись исходного датасета.
+    y : numpy.ndarray
+        Данные целевой колонки
+    n_jobs : int
+        Количество одновременно запускаемых параллельно задач.
+    n_folds : int
+        Параметр кроссвалидации
+    score_func : callable, optional
+        Функция оценки результата, принимающая векторы Y и Y' (цель обучения и предсказания).
+        При отсутствии используется clf.score
+
+    Returns
+    -------
+    sklearn.base.ClassifierMixin
+        Классификатор с оптимальными (из предоставленных) гиперпараметрами.
+    """
     if score_func:
         gs = GridSearchCV(clf, param_grid=parameters, cv=n_folds, n_jobs=n_jobs, scoring=score_func)
     else:
@@ -23,6 +52,44 @@ def cv_optimize(clf, parameters, X, y, n_jobs=1, n_folds=5, score_func=None):
 
 def do_classify(clf, parameters, indf, featurenames, targetname, target1val, mask=None,
                 reuse_split=None, score_func=None, n_folds=5, n_jobs=1):
+    """ Провести бинарную классификацию данных с оптимальными параметрами и вывести оценку точности
+    классификатора на тренировочных и тестовых данных и confusion matrix.
+
+    Parameters
+    ----------
+    clf : sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin
+        Объект-классификатор.
+    parameters : dict
+        Перебираемые параметры в виде {параметр1:[значение1_1, значение1_2],параметр2:[...]}
+    indf : pandas.DataFrame
+        Данные для обучения
+    featurenames : list of str
+        Заголовки колонок датасета, которые мы используем для обучения
+    targetname : str
+        Заголовок целевой колонки
+    target1val : str
+        Значение в целевой колонке которое будет считаться классом 1 (остальные - 0)
+    mask : list of bool, optional
+        Маска для отделения тестовых данных, от данных обучения.
+        True для строк которые будут использованы для обучения, False для оценки.
+        При использовании reuse_split игнорируется
+    reuse_split : dict, optional
+        Использовать предварительно разделенные данные, ключи Xtrain, Xtest, ytrain, ytest.
+    score_func : callable, optional
+        Функция оценки результата, принимающая векторы Y и Y' (цель обучения и предсказания).
+        При отсутствии используется clf.score
+    n_jobs : int
+        Количество одновременно запускаемых параллельно задач.
+    n_folds : int
+        Параметр кроссвалидации
+
+    Returns
+    -------
+    clf : sklearn.base.ClassifierMixin
+        Обученый классификатор.
+    Xtrain, ytrain, Xtest, ytest : numpy.ndarray
+        Данные использованного деления.
+    """
     subdf = indf[featurenames]
     X = subdf.values
     y = (indf[targetname].values == target1val) * 1
@@ -47,39 +114,88 @@ def do_classify(clf, parameters, indf, featurenames, targetname, target1val, mas
     return clf, Xtrain, ytrain, Xtest, ytest
 
 
-def points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=True, colorscale=cmap_light, cdiscrete=cmap_bold,
-                alpha=0.3, psize=10, zfunc=False):
-    h = .02
+def points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=True, alpha=0.3, psize=10):
+    """ Вывести диаграмму рассеяния по двум первым переменным датасета
+    с визуализацией классификатора.
+    Точки тренировочных данных обозначены кружками, тестовых - квадратами.
+
+    Parameters
+    ----------
+    ax : matplotlib.axis.Axes
+        Текущий график.
+    Xtr : numpy.ndarray
+        Тренировочные данные признаков.
+    Xte : numpy.ndarray
+        Тестовые данные признаков.
+    ytr : numpy.ndarray
+        Тренировочные данные целевой колонки.
+    yte : numpy.ndarray
+        Тестовые данные целевой колонки.
+    clf : sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin
+        Визуализируемый классификатор.
+    mesh : bool
+        Выводить ли сетку на фоне?
+    alpha : float
+        Мера непрозрачности точек, от 0 до 1
+    psize : int
+        Размер точек данных
+
+    Returns
+    -------
+    ax : matplotlib.axis.Axes
+        Текущий график.
+    xx, yy : numpy.ndarray
+        Значения координатной сетки.
+    """
     X = np.concatenate((Xtr, Xte))
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
                          np.linspace(y_min, y_max, 100))
-
-    # plt.figure(figsize=(10,6))
     if mesh:
-        if zfunc:
-            p0 = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 0]
-            p1 = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-            Z = zfunc(p0, p1)
-        else:
-            Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
         plt.pcolormesh(xx, yy, Z, cmap=cmap_light, alpha=alpha, axes=ax)
     ax.scatter(Xtr[:, 0], Xtr[:, 1], c=ytr - 1, cmap=cmap_bold, s=psize, alpha=alpha, edgecolor="k")
-    # and testing points
-    yact = clf.predict(Xte)
-    ax.scatter(Xte[:, 0], Xte[:, 1], c=yte - 1, cmap=cmap_bold, alpha=alpha, marker="s",
-               s=psize + 10)
+    ax.scatter(Xte[:, 0], Xte[:, 1], c=yte - 1, cmap=cmap_bold, s=psize + 10, alpha=alpha, marker="s")
     ax.set_xlim(xx.min(), xx.max())
     ax.set_ylim(yy.min(), yy.max())
     return ax, xx, yy
 
 
-def points_plot_prob(ax, Xtr, Xte, ytr, yte, clf, colorscale=cmap_light, cdiscrete=cmap_bold,
+def points_plot_prob(ax, Xtr, Xte, ytr, yte, clf,
                      ccolor=cm, psize=10, alpha=0.1, prob=True):
-    ax, xx, yy = points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=False, colorscale=colorscale,
-                             cdiscrete=cdiscrete, psize=psize, alpha=alpha)
+    """
+
+    Parameters
+    ----------
+    ax : matplotlib.axis.Axes
+        Текущий график.
+    Xtr : numpy.ndarray
+        Тренировочные данные признаков.
+    Xte : numpy.ndarray
+        Тестовые данные признаков.
+    ytr : numpy.ndarray
+        Тренировочные данные целевой колонки.
+    yte : numpy.ndarray
+        Тестовые данные целевой колонки.
+    clf : sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin
+        Визуализируемый классификатор.
+    ccolor : colormap
+        Цветовая схема для линий контуров
+    alpha : float
+        Мера непрозрачности точек, от 0 до 1
+    psize : int
+        Размер точек данных
+    prob : bool
+        True для классификаторов возвращающих вероятность, False - для дискриминаторов.
+
+    Returns
+    -------
+    ax : matplotlib.axis.Axes
+        Текущий график.
+    """
+    ax, xx, yy = points_plot(ax, Xtr, Xte, ytr, yte, clf, mesh=False, psize=psize, alpha=alpha)
     if prob:
         Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
     else:
